413d27df4aba2ce1a330b64cf43dccaaff8e14d2
Trey Springer <dfsiii@gmail.com>
2011-11-30 09:36:18 -0500

    Initial Commit

---
+++ LessBasicGlasses/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  LessBasicGlasses
+//
+//  Created by Trey Springer on 11/30/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ LessBasicGlasses/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LessBasicGlasses
+//
+//  Created by Trey Springer on 11/30/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
